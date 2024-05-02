fcdf29e9642e218615c367b02b56d892d1b1880d
Andre H <andrehadiantolesmana@gmail.com>
2024-05-02 14:46:01 +0800

    🔧 chore: add prettierrc

---
+++ .prettierrc.js
@@ -0,0 +1,17 @@
+module.exports = {
+  plugins: ["@trivago/prettier-plugin-sort-imports"],
+  semi: false,
+  trailingComma: "all",
+  printWidth: 100,
+  singleQuote: true,
+  tabWidth: 2,
+  arrowParens: "always",
+  importOrder: [
+    "^@snowballtools/(.*)$",
+    "<THIRD_PARTY_MODULES>",
+    "^[./]",
+    "^[../]",
+  ],
+  importOrderSeparation: true,
+  importOrderSortSpecifiers: true,
+};
